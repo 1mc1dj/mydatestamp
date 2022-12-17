103b264fd8d606074127f29670b77a421f4d8d20
DYNAM <we_love_blog@yahoo.com>
2022-12-17 14:51:18 +0000

    modify code style

--- src/main.rs
+++ src/main.rs
@@ -38,82 +38,81 @@ fn main() {
     print!("top line 1:{}", top_line_str);
     print!("top line 2:{}", top_line2_str);
     print!("username:{}", username_str);
-
     let filename = &args[1];
 
     let center_circle = Data::new()
-    .move_to((105, 30))
-    .cubic_curve_by(((-75,0),(-75,75),(-75,75)))
-    .cubic_curve_by(((0,75),(75,75),(75,75)))
-    .cubic_curve_by(((75,0),(75,-75),(75,-75)))
-    .cubic_curve_by(((0,-75),(-75,-75),(-75,-75))).close();
+        .move_to((105, 30))
+        .cubic_curve_by(((-75,0),(-75,75),(-75,75)))
+        .cubic_curve_by(((0,75),(75,75),(75,75)))
+        .cubic_curve_by(((75,0),(75,-75),(75,-75)))
+        .cubic_curve_by(((0,-75),(-75,-75),(-75,-75))).close();
 
     let top_line = Data::new()
-    .move_to((11*3,28*3))
-    .line_by((48*3,0));
+        .move_to((11*3,28*3))
+        .line_by((48*3,0));
 
     let bottom_line = Data::new()
-    .move_to((11*3,42*3))
-    .line_by((48*3,00));
+        .move_to((11*3,42*3))
+        .line_by((48*3,00));
 
     let center_circle_path = Path::new()
-    .set("fill", "none")
-    .set("stroke", "red")
-    .set("stroke-width", 3.6)
-    .set("d", center_circle);
+        .set("fill", "none")
+        .set("stroke", "red")
+        .set("stroke-width", 3.6)
+        .set("d", center_circle);
 
     let top_line_path = center_circle_path.clone()
-    .set("d", top_line);
+        .set("d", top_line);
 
     let bottom_line_path = top_line_path.clone()
-    .set("d", bottom_line);
+        .set("d", bottom_line);
 
     let date_str = Local::now().format("%Y/%m/%d").to_string();
     let text = svg::node::Text::new(date_str);
     let date_text = svg::node::element::Text::new()
-    .set("font-family", "Lucida-Console")
-    .set("font-size", 23)
-    .set("fill", "red")
-    .set("x",12*3)
-    .set("y",37*3)
-    .add(text);
+        .set("font-family", "Lucida-Console")
+        .set("font-size", 23)
+        .set("fill", "red")
+        .set("x",12*3)
+        .set("y",37*3)
+        .add(text);
 
     let top_text = svg::node::Text::new(top_line_str);
     let top_text_label = svg::node::element::Text::new()
-    .set("font-family", "sans-serif")
-    .set("font-size", 18)
-    .set("fill", "red")
-    .set("x",63)
-    .set("y",60)
-    .add(top_text);
+        .set("font-family", "sans-serif")
+        .set("font-size", 18)
+        .set("fill", "red")
+        .set("x",63)
+        .set("y",60)
+        .add(top_text);
 
     let top_text2 = svg::node::Text::new(top_line2_str);
     let top_text_label2 = svg::node::element::Text::new()
-    .set("font-family", "sans-serif")
-    .set("font-size", 16.5)
-    .set("fill", "red")
-    .set("x",46)
-    .set("y",78)
-    .add(top_text2);
+        .set("font-family", "sans-serif")
+        .set("font-size", 16.5)
+        .set("fill", "red")
+        .set("x",46)
+        .set("y",78)
+        .add(top_text2);
 
     let bottom_text = svg::node::Text::new(username_str);
     let bottom_label = svg::node::element::Text::new()
-    .set("font-family", "serif")
-    .set("font-size", 21)
-    .set("fill", "red")
-    .set("x",18*3)
-    .set("y",52*3)
-    .add(bottom_text);
+        .set("font-family", "serif")
+        .set("font-size", 21)
+        .set("fill", "red")
+        .set("x",18*3)
+        .set("y",52*3)
+        .add(bottom_text);
 
     let document = Document::new()
-    .set("viewBox", (0, 0, 210, 210))
-    .add(center_circle_path)
-    .add(top_line_path)
-    .add(bottom_line_path)
-    .add(date_text)
-    .add(top_text_label)
-    .add(top_text_label2)
-    .add(bottom_label);
+        .set("viewBox", (0, 0, 210, 210))
+        .add(center_circle_path)
+        .add(top_line_path)
+        .add(bottom_line_path)
+        .add(date_text)
+        .add(top_text_label)
+        .add(top_text_label2)
+        .add(bottom_label);
 
     svg::save(filename.to_owned(), &document).unwrap();
     println!("wrote {} file", filename);
