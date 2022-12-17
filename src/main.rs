4e3e2f54b0404d421017767ed432f4035761d9a9
Tomoo <moo@tmiz.net>
2022-12-18 00:07:35 +0900

    refactoring and format rust code

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
 
     // read config files
     let home = env::var("HOME").unwrap();
-    let config_file_path  = format!("{}/.mydatestamp", home);
+    let config_file_path = format!("{}/.mydatestamp", home);
     if std::path::Path::new(&config_file_path).exists() == false {
         println!("prepare property file(~/.mydatestamp)");
         return;
@@ -42,18 +42,15 @@ fn main() {
 
     let center_circle = Data::new()
         .move_to((105, 30))
-        .cubic_curve_by(((-75,0),(-75,75),(-75,75)))
-        .cubic_curve_by(((0,75),(75,75),(75,75)))
-        .cubic_curve_by(((75,0),(75,-75),(75,-75)))
-        .cubic_curve_by(((0,-75),(-75,-75),(-75,-75))).close();
+        .cubic_curve_by(((-75, 0), (-75, 75), (-75, 75)))
+        .cubic_curve_by(((0, 75), (75, 75), (75, 75)))
+        .cubic_curve_by(((75, 0), (75, -75), (75, -75)))
+        .cubic_curve_by(((0, -75), (-75, -75), (-75, -75)))
+        .close();
 
-    let top_line = Data::new()
-        .move_to((11*3,28*3))
-        .line_by((48*3,0));
+    let top_line = Data::new().move_to((11 * 3, 28 * 3)).line_by((48 * 3, 0));
 
-    let bottom_line = Data::new()
-        .move_to((11*3,42*3))
-        .line_by((48*3,00));
+    let bottom_line = Data::new().move_to((11 * 3, 42 * 3)).line_by((48 * 3, 00));
 
     let center_circle_path = Path::new()
         .set("fill", "none")
@@ -61,47 +58,43 @@ fn main() {
         .set("stroke-width", 3.6)
         .set("d", center_circle);
 
-    let top_line_path = center_circle_path.clone()
-        .set("d", top_line);
+    let top_line_path = center_circle_path.clone().set("d", top_line);
 
-    let bottom_line_path = top_line_path.clone()
-        .set("d", bottom_line);
+    let bottom_line_path = top_line_path.clone().set("d", bottom_line);
+
+    let text_base = svg::node::element::Text::new()
+        .set("font-family", "sans-serif")
+        .set("font-size", 22)
+        .set("fill", "red");
 
     let date_str = Local::now().format("%Y/%m/%d").to_string();
-    let text = svg::node::Text::new(date_str);
-    let date_text = svg::node::element::Text::new()
-        .set("font-family", "Lucida-Console")
-        .set("font-size", 23)
-        .set("fill", "red")
-        .set("x",12*3)
-        .set("y",37*3)
-        .add(text);
+    let date_text = svg::node::Text::new(date_str);
+    let date_label = text_base.clone().set("x", 12 * 3).set("y", 37 * 3).add(
+        date_text,
+    );
 
     let top_text = svg::node::Text::new(top_line_str);
-    let top_text_label = svg::node::element::Text::new()
-        .set("font-family", "sans-serif")
-        .set("font-size", 18)
-        .set("fill", "red")
-        .set("x",63)
-        .set("y",60)
+    let top_text_label = text_base
+        .clone()
+        .set("x", 63)
+        .set("y", 60)
+        .set("font-size", 20)
         .add(top_text);
 
     let top_text2 = svg::node::Text::new(top_line2_str);
-    let top_text_label2 = svg::node::element::Text::new()
-        .set("font-family", "sans-serif")
-        .set("font-size", 16.5)
-        .set("fill", "red")
-        .set("x",46)
-        .set("y",78)
+    let top_text_label2 = text_base
+        .clone()
+        .set("x", 46)
+        .set("y", 78)
+        .set("font-size", 18)
         .add(top_text2);
 
     let bottom_text = svg::node::Text::new(username_str);
-    let bottom_label = svg::node::element::Text::new()
+    let bottom_label = text_base
+        .clone()
         .set("font-family", "serif")
-        .set("font-size", 21)
-        .set("fill", "red")
-        .set("x",18*3)
-        .set("y",52*3)
+        .set("x", 18 * 3)
+        .set("y", 52 * 3)
         .add(bottom_text);
 
     let document = Document::new()
@@ -109,7 +102,7 @@ fn main() {
         .add(center_circle_path)
         .add(top_line_path)
         .add(bottom_line_path)
-        .add(date_text)
+        .add(date_label)
         .add(top_text_label)
         .add(top_text_label2)
         .add(bottom_label);
